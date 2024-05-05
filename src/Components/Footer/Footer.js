ec56fa184d917f39b1666b81e7f5c979337b439d
nadeemahmed12 <nadeemahmed9480@gmail.com>
2024-05-05 12:22:28 +0530

    Initial Commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import footerStyle from './Footer.module.css';
+
+const Footer = ()=>{
+    return(
+        <footer className={footerStyle.footer}>Created By : Nadeem Ahmed
+        </footer>
+     )
+}
+
+export default Footer;
